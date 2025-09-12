d0af602f2948617d0c839d7f3476a9cf8628a55c
Karnphong Saratontiphong <inseekan@gmail.com>
2025-09-13 06:12:24 +0700

    fix main

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from edit.vid import get_vids
 from edit.edit import create_video
 from yt.upload import run_upload
 from notify.discord import discord_message
-import asyncio
 
-async def main():
-    get_vids()          
-    create_video()            
-    video_url = run_upload()
-    if video_url:
-        print("upload step pass")
-        user_id = "304548816907010050"
-        discord_message(f"✅ <@{user_id}> Uploaded vids: {video_url} ✅")
-    else:
-        print("⚠️ Upload failed, skipping Discord notify")
 
 if __name__ == "__main__":
-    asyncio.run(main())
 
+    get_vids()
+
+
+    import os
+    BG_FOLDER = "src/bg"
+    video_files = [f for f in os.listdir(BG_FOLDER) if f.endswith(".mp4")]
+    if len(video_files) < 1:
+        raise RuntimeError(f"❌ BG files not ready. Found {len(video_files)} clips.")
+
+
+    create_video()
+
+    video_url = run_upload()
+    if video_url:
+        user_id = "304548816907010050"
+        discord_message(f"✅ <@{user_id}> Uploaded vids: {video_url} ✅")
